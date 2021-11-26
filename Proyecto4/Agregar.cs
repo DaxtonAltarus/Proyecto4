011b67dc05bde5679a40870943f3098050737f94
DaxtonAltarus <84556668+DaxtonAltarus@users.noreply.github.com>
2021-11-26 16:33:29 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto4/Agregar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto4
+{
+    public partial class Agregar : Form
+    {
+        public Agregar()
+        {
+            InitializeComponent();
+        }
+
+        private void Agregar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Proyecto4/aclase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto4
+{
+    public partial class aclase : Form
+    {
+        public aclase()
+        {
+            InitializeComponent();
+        }
+    }
+}
